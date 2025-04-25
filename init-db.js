345b3a6915524cf54d139ef1b885cc40d750d20c
Ashton Wooster <woosterashton48@gmail.com>
2025-04-25 00:17:25 -0500

    database init, WIP

--- init-db.js
+++ init-db.js
@@ -8,9 +8,68 @@ console.log('Initializing DB...');
 db.exec(`
   CREATE TABLE IF NOT EXISTS nonadminuser (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    address TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+  );
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS administrator (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    datehired DATE NOT NULL,
+    datefinished DATE
+  );
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS accountcategory (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) TEXT NOT NULL
+  );
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS group (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL
+  );
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS masteraccount (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    openingamount DOUBLE NOT NULL,
+    closingamount DOUBLE
+  );
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS transaction (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date DATE NOT NULL,
+    description VARCHAR(255) NOT NULL
+  );
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS transactionline (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    creditedamount DOUBLE NOT NULL,
+    debitedamount DOUBLE NOT NULL,
+    comments VARCHAR(255) NOT NULL
+  );
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS userpassword (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    encryptedpassword VARCHAR(255) NOT NULL,
+    passwordexpirytime INT NOT NULL,
+    useraccountexpirydate DATE NOT NULL
   );
 `);
 
