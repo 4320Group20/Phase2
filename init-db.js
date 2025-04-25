d050e4d0bef80a65b1badd6ccf47be5370790ecc
Ashton Wooster <woosterashton48@gmail.com>
2025-04-25 01:05:34 -0500

    database relations

--- init-db.js
+++ init-db.js
@@ -1,25 +1,9 @@
-// Open Database
 const Database = require('better-sqlite3');
 const db = new Database('./database.db');
 
 console.log('Initializing DB...');
 
-// Initialize tables
-db.exec(`
-  CREATE TABLE IF NOT EXISTS accountcategory (
-    accountcategory_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255) NOT NULL,
-    type VARCHAR(255) NOT NULL
-  );
-`);
-
-db.exec(`
-  CREATE TABLE IF NOT EXISTS "group" (
-    group_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255) NOT NULL
-  );
-`);
-
+// Creating tables
 db.exec(`
   CREATE TABLE IF NOT EXISTS masteraccount (
     masteraccount_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -30,19 +14,13 @@ db.exec(`
 `);
 
 db.exec(`
-  CREATE TABLE IF NOT EXISTS "transaction" (
-    transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    date DATE NOT NULL,
-    description VARCHAR(255) NOT NULL
-  );
-`);
-
-db.exec(`
-  CREATE TABLE IF NOT EXISTS transactionline (
-    transactionline_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    credited_amount DOUBLE NOT NULL,
-    debited_amount DOUBLE NOT NULL,
-    comments VARCHAR(255) NOT NULL
+  CREATE TABLE IF NOT EXISTS "group" (
+    group_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    parent_masteraccount_id INTEGER NOT NULL,
+    parent_group_id INTEGER NOT NULL,
+    FOREIGN KEY (parent_masteraccount_id) REFERENCES masteraccount(masteraccount_id),
+    FOREIGN KEY (parent_group_id) REFERENCES "group"(group_id)
   );
 `);
 
@@ -62,7 +40,7 @@ db.exec(`
     address VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     userpassword_id INTEGER NOT NULL,
-    FOREIGN KEY (userpassword_id) REFERENCES userpassword(userpassword_id) 
+    FOREIGN KEY (userpassword_id) REFERENCES userpassword(userpassword_id)
   );
 `);
 
@@ -71,9 +49,44 @@ db.exec(`
     administrator_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(255) NOT NULL,
     date_hired DATE NOT NULL,
-    date_finished DATE
+    date_finished DATE,
     userpassword_id INTEGER NOT NULL,
     FOREIGN KEY (userpassword_id) REFERENCES userpassword(userpassword_id)
+  );
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS accountcategory (
+    accountcategory_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    group_id INTEGER NOT NULL,
+    FOREIGN KEY (group_id) REFERENCES "group"(group_id)
+  );
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS "transaction" (
+    transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date DATE NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES nonadminuser(nonadminuser_id)
+  );
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS transactionline (
+    transactionline_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    credited_amount DOUBLE NOT NULL,
+    debited_amount DOUBLE NOT NULL,
+    comments VARCHAR(255) NOT NULL,
+    transaction_id INTEGER NOT NULL,
+    first_masteraccount_id INTEGER NOT NULL,
+    second_masteraccount_id INTEGER NOT NULL,
+    FOREIGN KEY (transaction_id) REFERENCES "transaction"(transaction_id),
+    FOREIGN KEY (first_masteraccount_id) REFERENCES masteraccount(masteraccount_id),
+    FOREIGN KEY (second_masteraccount_id) REFERENCES masteraccount(masteraccount_id)
   );
 `);
 
