881c7306f0ee7649099a2a2fe0c5442e22700870
Ashton Wooster <woosterashton48@gmail.com>
2025-04-27 18:02:31 -0500

    Fix to init file

--- init.sh
+++ init.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-echo " Installing root dependencies…"
+echo " Installing root dependencies"
 npm install
 
-echo " Initializing DB…"
-node init-db.js
+echo " Initializing DB"
 if [ "$1" == "y" ]; then
+  node init-db.js
   node populate-db.js
 fi
 
-echo " Installing client dependencies…"
-( cd client && npm install )
+echo " Installing client dependencies"
+cd client || exit
+npm install
+cd ..
 
-echo " Starting both API and React client…"
+echo " Starting both API and React client"
 npm run dev
